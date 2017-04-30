07ae5880363c6a2428fbef5028f99c76d9ff9482
KOMP\Shellest <szelest88@gmail.com>
2017-04-30 22:06:52 +0200

    Create a simple forms project.

---
+++ Raytracer/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Raytracer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(800, 600);
+            for(int i=0;i<500;i++)
+                for(int j=0;j<500;j++)
+            bmp.SetPixel(i,j, Color.Red);
+            pictureBox1.Image = bmp;
+        }
+    }
+}
