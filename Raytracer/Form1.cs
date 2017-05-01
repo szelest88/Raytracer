ec658b63065ad8944c50092657c3cc13b18187b1
KOMP\Shellest <szelest88@gmail.com>
2017-05-01 19:40:27 +0200

    Create some very basic classes.

--- Raytracer/Form1.cs
+++ Raytracer/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 
 namespace Raytracer
 {
+ 
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,11 +21,65 @@ namespace Raytracer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Bitmap bmp = new Bitmap(800, 600);
+            Bitmap bmp = new Bitmap(800, 800);
             for(int i=0;i<800;i++)
-                for(int j=0;j<600;j++)
-            bmp.SetPixel(i,j, Color.Red);
+                for(int j=0;j<800;j++)
+            bmp.SetPixel(i,j, Color.DarkGray);
             pictureBox1.Image = bmp;
+
+            // let's say... kamera w 0,0,0, patrzy na 0,0,1
+            Sphere s1 = new Sphere() // VS hinted me this syntax... I didn't know it.
+            {
+                center = new Vector3(0, 0, 1),
+                radius = 0.2f
+            };
+
+            Camera cam = new Camera(new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 0));
+
         }
+    }
+
+    public class Vector3
+    {
+        public float x, y, z;
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    public class Sphere
+    {
+        public Vector3 center;
+        public float radius;
+    }
+
+    public class Ray
+    {
+        Vector3 begin;
+        Vector3 direction;
+
+        public Ray(Vector3 begin, Vector3 direction)
+        {
+            this.begin = begin;
+            this.direction = direction;
+        }
+    }
+
+    public class Camera
+    {
+        public Vector3 position;
+        public Vector3 direction;
+        public Vector3 up;
+
+        public Camera(Vector3 position, Vector3 direction, Vector3 up)
+        {
+            this.position = position;
+            this.direction = direction;
+            this.up = up;
+        }
+
     }
 }
